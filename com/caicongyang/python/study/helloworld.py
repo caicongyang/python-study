fea06104089fdb9179c17c11eeba8719e1b095e9
caicongyang <caicongyang@crop.netease.com>
2019-05-11 14:13:14 +0800

    基础

--- com/caicongyang/python/study/helloworld.py
+++ com/caicongyang/python/study/helloworld.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 print('hello,world')
 
 print('-----------变量使用：-----------------')
@@ -57,4 +59,42 @@ print(list1)
 
 
 
+print('-----------tuple-----------------')
+tup1 = (12, 34.56)
+tup2 = ('abc', 'xyz')
 
+print(tup1)
+
+# 以下修改元组元素操作是非法的。
+# tup1[0] = 100
+
+# 创建一个新的元组
+tup3 = tup1 + tup2
+print (tup3)
+
+
+
+print('-----------dictionary字典-----------------')
+score ={"jack":"alibaba","pony":"tencent"}
+score["willam"]="netease"
+print(score)
+score.pop('jack')
+
+#获取字段的内的值
+print(score.get("jack"))
+
+
+
+print('-----------set--------------------------')
+s = set(['a','b','c'])
+s.add('a')
+print(s)
+print('c' in s)
+
+
+print('-----------function--------------------------')
+def add(a,b):
+    return  a + b
+
+print(add('a',"b"))
+print(add(3,4))
