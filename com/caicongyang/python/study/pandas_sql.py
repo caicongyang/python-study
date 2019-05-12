715fc070cefc5b887481c1ce8a7bd3ca53a86334
caicongyang <caicongyang@crop.netease.com>
2019-05-12 13:43:52 +0800

    add pandas sql test case

---
+++ com/caicongyang/python/study/pandas_sql.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import numpy as np
+import pandas as pd
+
+from pandas import DataFrame, Series
+from pandasql import sqldf,load_meat,load_births
+
+df1=DataFrame({'name':['jack','tony','pony'],'data1':range(3)})
+print(df1)
+
+
+sql = "select * from df1 where name = 'jack'"
+pysqldf = lambda  sql :sqldf(sql,globals());
+print(pysqldf(sql))
