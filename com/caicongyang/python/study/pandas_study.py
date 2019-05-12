d68373abce0dce2338e7ff585270855bc9938038
caicongyang <caicongyang@crop.netease.com>
2019-05-12 11:36:26 +0800

    add some pandas test case

--- com/caicongyang/python/study/pandas_study.py
+++ com/caicongyang/python/study/pandas_study.py
@@ -31,3 +31,31 @@ df1=DataFrame(data)
 print(df1)
 df2=DataFrame(data,index=['jack','pony','william'],columns=['chinese','math','english'])
 print(df2)
+
+data2={'science':[100,100,100],'geograpy':[100,100,100]}
+df3=DataFrame(data2,index=['jack','pony','william'],columns=['science','geograpy'])
+
+print(df3)
+
+#append
+df4=df2.append(df3,sort=False)
+print(df4)
+
+
+
+
+#merage
+df5=pd.merge(df2,df3,left_index=True, right_index=True, how='outer')
+print(df5)
+
+'''
+merage result
+
+         chinese  math  english  science  geograpy
+jack          99    99       60      100       100
+pony          88   100      100      100       100
+william      100   100      100      100       100
+
+'''
+
+
