3eb3d6c15518d33e78f77b19c5c03221ae2639f2
caicongyang <caicongyang@crop.netease.com>
2019-05-12 13:46:46 +0800

    add some pandas sql test case

--- com/caicongyang/python/study/pandas_sql.py
+++ com/caicongyang/python/study/pandas_sql.py
@@ -4,11 +4,17 @@
 import numpy as np
 import pandas as pd
 
+
+'''
+如何用sql的方式打开pandas
+'''
+
 from pandas import DataFrame, Series
 from pandasql import sqldf,load_meat,load_births
 
 df1=DataFrame({'name':['jack','tony','pony'],'data1':range(3)})
 print(df1)
+
 
 
 sql = "select * from df1 where name = 'jack'"
